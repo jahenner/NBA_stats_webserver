9fcba622a5c57651d9747ddd5c6ad34067f98d7d
Alex Henner <jahenner@gmail.com>
2022-05-03 19:49:57 +0000

    Finished Players layout

--- nba_stats/src/pages/Players.js
+++ nba_stats/src/pages/Players.js
@@ -24,6 +24,46 @@ function Players() {
                     <PlayersTable />
                 </tbody>
             </table>
+            <form id="addPlayers">
+                <legend><strong>Add Player</strong></legend>
+                <fieldset class="fields">
+                    <label> First Name </label> <input type="text" name="first_name" />
+                    <label> Last Name </label> <input type="text" name="last_name" />
+                    <label> Age </label> <input type="number" name="age" />
+                    <label> Career Points </label> <input type="number" name="career_points" />
+                    <label> Career Steal </label> <input type="number" name="career_steals" />
+                    <label> Career Blocks </label> <input type="number" name="career_blocks" />
+                    <label> Career Rebounds </label> <input type="number" name="career_rebounds" />
+                    <label> Hometown </label> <select name="city_id">
+                        <CityOptions />
+                    </select>
+                    <label> Current Team </label> <select name="team_id">
+                        <TeamOptions />
+                    </select>
+                </fieldset>
+                <input class="btn" type="submit" id="addPlayerss" value="Add a Player"></input>
+                <input class="btn" type="button" value="cancel"></input>
+            </form>
+            <form id="editPlayers">
+                <legend><strong>Edit Player (use the edit icon next to the Player you would like to edit)</strong></legend>
+                <fieldset class="fields">
+                    <label> First Name </label> <input type="text" name="first_name" />
+                    <label> Last Name </label> <input type="text" name="last_name" />
+                    <label> Age </label> <input type="number" name="age" />
+                    <label> Career Points </label> <input type="number" name="career_points" />
+                    <label> Career Steal </label> <input type="number" name="career_steals" />
+                    <label> Career Blocks </label> <input type="number" name="career_blocks" />
+                    <label> Career Rebounds </label> <input type="number" name="career_rebounds" />
+                    <label> Hometown </label> <select name="city_id">
+                        <CityOptions />
+                    </select>
+                    <label> Current Team </label> <select name="team_id">
+                        <TeamOptions />
+                    </select>
+                </fieldset>
+                <input class="btn" type="submit" id="editPlayerss" value="Edit a Player"></input>
+                <input class="btn" type="button" value="cancel"></input>
+            </form>
         </article>
         
     );
