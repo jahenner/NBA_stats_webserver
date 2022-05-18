81a15bb6e0599e77471dfa5a337a5ff51c6b2514
Alex Henner <jahenner@gmail.com>
2022-05-18 22:48:55 +0000

    working on proxy

--- nba_stats/setupProxy.js
+++ nba_stats/setupProxy.js
@@ -1,11 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      changeOrigin: true,
-    })
-  );
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+      pathRewrite: { '^/api': '' }
+    }
+  }
 };
